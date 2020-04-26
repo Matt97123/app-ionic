77502dc2a15adff5e8c072cd302db2c5d3cd40fe
HUBBEL Matthieu <matt_hubbel@live.fr>
2020-04-26 19:48:56 +0200

    ajout des services de filtre

--- src/app/services/fruit.service.ts
+++ src/app/services/fruit.service.ts
@@ -49,7 +49,7 @@ export class FruitService {
     });
   }
 
-  public async searchByLetter(regex) {
+  public async Filter(regex, weightFilter, sortBy) {
     
     switch (regex) {
       case 'A..F' : regex = /^[a-f]*?$/;
@@ -65,13 +65,55 @@ export class FruitService {
     this.fruits.forEach(element => {
       if (!element.nom.charAt(0).match(regex)) {
         this.fruits = this.fruits.filter(f => f !== element);
+        this.searchByWeight(weightFilter);
       }
     });
+    if (sortBy == 'ordre alphabétique') {
+      this.sortByAlpha();
+    } else if (sortBy == 'poids croissant') {
+      this.sortByWeight();
+    }
+    
     this.location.back();
   }
 
-  public async searchByWeight() {
- 
+  public async searchByWeight(weightFilter) {
+    this.fruits.forEach(element => {
+      switch (weightFilter) {
+        case 'inférieur à 2 kg' : if (element.poids > 2) {
+          this.fruits = this.fruits.filter(f => f !== element);
+        }
+        break;
+        case 'entre 2 et 5 kg' : if (element.poids < 2 || element.poids > 5) {
+          this.fruits = this.fruits.filter(f => f !== element);
+        }
+        break;
+        case 'entre 5 et 10 kg' : if (element.poids < 5 || element.poids > 10) {
+          this.fruits = this.fruits.filter(f => f !== element);
+        }
+        break;
+        case 'plus de 10 kg...' : if (element.poids < 10) {
+          this.fruits = this.fruits.filter(f => f !== element);
+        }
+        break;
+        default : if (element.poids < 0) {
+          this.fruits = this.fruits.filter(f => f !== element);
+        }
+      }
+    });
+  }
+
+  public async sortByAlpha() {
+    this.fruits = this.fruits.sort(function compare(a, b) {
+      if (a.nom < b.nom)
+         return -1;
+      if (a.nom > b.nom )
+         return 1;
+      return 0;
+    });
+  }
+  public async sortByWeight() {
+    this.fruits = this.fruits.sort((a, b) => a.poids - b.poids);
   }
 
   public async refresh() {
